f8f0be03594d150a4b366a23c6719b4c7f942acd
drobotmaksym <drobotmaxiim@gmail.com>
2023-10-14 11:41:12 +0200

    Initial commit

---
+++ src/app/provider.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { ThemeProvider } from "next-themes";
+
+const Provider = ({ children, ...props }) => {
+  return <ThemeProvider {...props}>{children}</ThemeProvider>;
+};
+
+export default Provider;
---
+++ src/components/ui/AspectRation.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio";
+
+const AspectRation = AspectRatioPrimitive.Root;
+
+export { AspectRation };
