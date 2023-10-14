9be29e0d769f7342bd1a1bc7f8991290f4c0ef57
tatallia <tatallia.nn@gmail.com>
2023-10-14 13:34:20 +0200

    Create page.tsx

---
+++ src/app/order/page.tsx
@@ -0,0 +1,11 @@
+import { Mode } from "@/components/Mode";
+
+export default function Home() {
+    return (
+        <main className="w-screen h-screen flex flex-col justify-center items-center gap-4">
+
+            <h1 className="grid place-content-center text-7xl font-bold">Hi, Mom!</h1>
+            <Mode />
+        </main>
+    );
+}
